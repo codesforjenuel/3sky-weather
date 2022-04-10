9515e6e315edea38e2f642b55dcde663f6adb507
codesforjenuel <codesforjenuel@gmail.com>
2022-04-09 23:49:05 -0700

    gave functionality to the search button to search a city info depending on the text typed in the input element

--- script.js
+++ script.js
@@ -18,10 +18,21 @@ document.querySelector(".temp").textContent = `Temp: ${weatherInfo.current.temp_
 document.querySelector(".wind").textContent = `Wind: ${weatherInfo.current.wind_mph} mph`
 document.querySelector(".humid").textContent = `Humidity: ${weatherInfo.current.humidity}`
 document.querySelector(".uv").textContent = `Uv: ${weatherInfo.current.uv}`
+document.querySelector(".condition").textContent = weatherInfo.current.condition.text
 //these query selectors target tomorrow's data
-document.querySelector(".tomorrow").textContent = weatherInfo.forecast.forecastday[0].date;
-
+document.querySelector(".tomorrow").textContent = weatherInfo.forecast.forecastday[1].date;   
+document.querySelector(".temp2").textContent = `Temp: ${weatherInfo.forecast.forecastday[1].hour[12].temp_f}°F`
+document.querySelector(".wind2").textContent = `Wind: ${weatherInfo.forecast.forecastday[1].hour[12].wind_mph} mph`
+document.querySelector(".humid2").textContent = `Humidity: ${weatherInfo.forecast.forecastday[1].hour[12].humidity}`
+document.querySelector(".uv2").textContent = `Uv: ${weatherInfo.forecast.forecastday[1].hour[12].uv}`
+document.querySelector(".condition2").textContent = weatherInfo.forecast.forecastday[1].hour[12].condition.text
 //these query selectors target the day after tomorrow's data
+document.querySelector(".dayAfterTomorrow").textContent = weatherInfo.forecast.forecastday[2].date;   
+document.querySelector(".temp3").textContent = `Temp: ${weatherInfo.forecast.forecastday[2].hour[12].temp_f}°F`
+document.querySelector(".wind3").textContent = `Wind: ${weatherInfo.forecast.forecastday[2].hour[12].wind_mph} mph`
+document.querySelector(".humid3").textContent = `Humidity: ${weatherInfo.forecast.forecastday[2].hour[12].humidity}`
+document.querySelector(".uv3").textContent = `Uv: ${weatherInfo.forecast.forecastday[2].hour[12].uv}`
+document.querySelector(".condition3").textContent = weatherInfo.forecast.forecastday[2].hour[12].condition.text
 }))} 
 const errorCallback = (error) => {
     console.error(error)
@@ -31,7 +42,6 @@ navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
 let btn = document.querySelector(".btn").addEventListener("click", () => {
     let inputEl = document.querySelector(".inputEl");
     let api = `https://api.weatherapi.com/v1/forecast.json?key=3ef0c6e33fa943f4805232459211609&q=${inputEl.value}&days=5&aqi=no&alerts=no`
-
 fetch(api)
 .then(response => response.json())
 .then((weatherData => {
